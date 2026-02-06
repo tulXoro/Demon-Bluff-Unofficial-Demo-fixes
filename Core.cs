62f3f0b56daff6a78fa420e230b238f03c00d299
Tim <tulxoro@hotmail.com>
2026-02-06 00:55:54 -0800

    docs: rename project and remove logging info.

--- Core.cs
+++ Core.cs
@@ -4,27 +4,18 @@ using Il2Cpp;
 using MelonLoader;
 
 
-[assembly: MelonInfo(typeof(Demon_Bluff_Unofficial_Demo_Patch.Core), "Demon Bluff tozeon's Unofficial Demo Patch", "0.1.0", "tulxoro", null)]
+[assembly: MelonInfo(typeof(Demon_Bluff_Unofficial_Demo_Fixes.Core), "Demon Bluff Demo Fixes", "0.1.0", "tulxoro", null)]
 [assembly: MelonGame("UmiArt", "Demon Bluff")]
 
-namespace Demon_Bluff_Unofficial_Demo_Patch
+namespace Demon_Bluff_Unofficial_Demo_Fixes
 {
     public class Core : MelonMod
     {
         public override void OnInitializeMelon()
         {
-            LoggerInstance.Msg("Initialized.");
-            
             // Apply the harmony patch
             var harmony = new HarmonyLib.Harmony("tozeon.Demon.Bluff.Patch");
             harmony.PatchAll();
-
-            GameEvents.OnGameplayStateChange += new Action(OnGameplayStateChange);
-        }
-
-        private static void OnGameplayStateChange() {
-             var currentState = Gameplay.GameplayState;
-            MelonLogger.Msg($"Gameplay State Changed to: {currentState}");
         }
     }
 
@@ -37,17 +28,22 @@ namespace Demon_Bluff_Unofficial_Demo_Patch
         {
 
             if (PlayerController.PlayerInfo != null && __instance == PlayerController.PlayerInfo.health)
-            {;
-                
+            {
                 if (__instance.value.GetValue() <= 0)
                 {
                     var winConditions = UnityEngine.Object.FindObjectOfType<WinConditions>();
 
-                    if (winConditions != null){
+                    if (winConditions != null)
                         winConditions.LoseNormal();
-                    }
-    
                     Gameplay.ChangeGameplayState(EGameplayState.Summary);
+
+                    Il2CppSystem.Collections.Generic.List<Character> allChars = new Il2CppSystem.Collections.Generic.List<Character>(Gameplay.CurrentCharacters.Pointer);
+
+                    for (int i = 0; i < allChars.Count; i++)
+                    {
+                        allChars[i].Reveal();
+                    }
+
                 }
             }
         }
---
+++ README.md
@@ -0,0 +1,34 @@
+# Demon Bluff - Unofficial Demo Fixes
+
+## Description
+
+This is an unofficial fix mod for the playtest of [Demon Bluff](https://store.steampowered.com/app/3522600/Demon_Bluff/), which provides small general fixes. It does not fix all bugs (and it likely won't ever try to), but if there is an important bug that helps mods or gameplay, this patch will try to eventually fix them.
+
+This mod is ***mod agnostic***. It may or may not work with other mods depending on how they are implemented.
+
+## Why?
+
+Although the game is still a ***demo/playtest***, I wanted to make small improvements for my own mods. Since I thought others might want the fixes, I decided to make it a separate stand-alone mod.
+
+## Current Fixes
+
+- Game not ending when health is low
+
+## Installation
+
+### ***Prerequisites***
+
+You will need to download [Melon Loader](https://melonwiki.xyz/#/).
+You will also need to game installed, of course.
+
+### Directions
+
+1. Open Melon Loader. Select/search "Demon Bluff Playtest".
+2. Enable Nightly builds and install.
+3. Launch the game first without any mods.
+4. Navigate to the game directory and you'll see the `mods` folder. Drag and drop this mod's `.dll` file in there and run the game.
+5. Enjoy the game.
+
+## Contributing
+
+Feel free to contribute. If there are any issues that you want fixed (with the this mod or with the demo) then open an issue for it.
